4f838f0ec8e6460e3016b266b257fe0470d40d1c
venkates-vk18 <royalvenky1434@gmail.com>
2026-04-21 12:27:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Success</title>
+    <meta charset="utf-8">
+    <title>Simple Deployment</title>
     <style>
         body {
-            background-color: green;
-            color: white;
+            color: #ffffff;
+            background-color: #0073bb;
+            font-family: Arial, sans-serif;
+            font-size: 14px;
             text-align: center;
-            padding-top: 200px;
-            font-family: Arial;
         }
+
         h1 {
-            font-size: 50px;
+            font-size: 500%;
+            font-weight: normal;
+            margin-bottom: 0;
+        }
+
+        h2 {
+            font-size: 200%;
+            font-weight: normal;
+            margin-bottom: 0;
         }
     </style>
 </head>
 <body>
-    <h1>Congratulations!</h1>
-    <p>You have successfully deployed using AWS CodePipeline & Elastic Beanstalk</p>
+    <div align="center">
+        <h1>Congratulations!</h1>
+        <h2>You have successfully created a pipeline that retrieved this source application from GitHub and deployed it to an Amazon EC2 instance using AWS CodePipeline.</h2>
+        <p>For next steps, see the AWS CodePipeline documentation.</p>
+    </div>
 </body>
 </html>
