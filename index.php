85b84a02ce9acdb217cf5aacf688b50800371c7f
venkates-vk18 <royalvenky1434@gmail.com>
2026-04-21 11:32:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-echo "Hello venkatesh - CI/CD Working";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Success</title>
+    <style>
+        body {
+            background-color: green;
+            color: white;
+            text-align: center;
+            padding-top: 200px;
+            font-family: Arial;
+        }
+        h1 {
+            font-size: 50px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Congratulations!</h1>
+    <p>You have successfully deployed using AWS CodePipeline & Elastic Beanstalk</p>
+</body>
+</html>
